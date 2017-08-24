c03932c0b3e7235989b4f0ebab6c64883c0b5a63
kendell joseph <kendell@jct.io>
2017-08-24 00:38:19 +0000

    Switched up headless browser tester

--- features/step_definitions/client.js
+++ features/step_definitions/client.js
@@ -3,6 +3,7 @@
 // ------------
 const dependencies = require(`${process.cwd()}/server_dependencies`);
 const chai         = dependencies.chai;
+const puppeteer    = dependencies.puppeteer;
 const should       = chai.should();
 const expect       = chai.expect;
 const {defineSupportCode: cucumber} = dependencies.cucumber;
@@ -10,35 +11,52 @@ const {defineSupportCode: cucumber} = dependencies.cucumber;
 // Request Actions
 // ---------------
 cucumber(({Given, When, Then}) => {
-  When(/^a client browser should start up$/, function(){
-    const seleniumWebdriver = require(`selenium-webdriver`);
-    this.webdriver          = seleniumWebdriver;
-    return this.driver    = new seleniumWebdriver.Builder()
-      .forBrowser(`chrome`)
-      .build();
+  When(/^a client browser should start up$/, function(done){
+    (async () => {
+      this.browser = await puppeteer.launch();
+      this.page    = await this.browser.newPage();
+      done();
+    })();
   });
 
-  When(/^a client browser should shut down$/, function(){
-    return this.driver.quit();
+  When(/^a client browser should shut down$/, function(done){
+    (async () => {
+      await this.browser.close();
+      done();
+    })();
   });
 
-  When(/^a client should be able to visit (.*)$/, function(path){
-    return this.driver.get(`localhost:${this.serverData.options.port}${path}`);
+  When(/^a client should be able to visit (.*)$/, function(path, done){
+    (async () => {
+      await this.page.goto(`http://localhost:${this.serverData.options.port}${path}`);
+      done();
+    })();
   });
 
-  When(/^a client should wait for an element tagged (.*)$/, function(tag){
-    const timeout = 5000;
-    return this.driver.wait(this.webdriver.until.elementLocated(this.webdriver.By.css(tag)), timeout)
+  When(/^a client should be able to see an element with the selector (.*)$/, function(tag){
+    return this.page.$(tag)
       .then((element) => {
-        expect(element).to.exist;
-        return element.isDisplayed().then((visible) => {
-          expect(visible).to.be.true;
+        return new Promise((resolve) => {
+          expect(element).to.exist;
+          resolve();
         });
       });
   });
 
+  When(/^a client should be able to see an element tagged (.*) containing the text value (.*)$/, function(tag, text){
+    return this.page.$(tag)
+      .then((element) => {
+        return new Promise((resolve, reject) => {
+          expect(element).to.exist;
 
-  When(/^a client should be able to see an element tagged (.*)$/, function(tag){
-    return this.driver.findElement(this.webdriver.By.css(`${tag}`));
+          element.evaluate((what) => {
+            return this.innerText;
+          }).then((result) => {
+            expect(result).to.equal(text);
+          })
+          .then(resolve)
+          .catch(reject);
+        });
+      });
   });
 });
--- features/test/client/demos.list.feature
+++ features/test/client/demos.list.feature
@@ -1,20 +1,20 @@
 Feature: The list of demos
-  
+
   Scenario: A person wants to see a list of demos available
     Given the server should start
       And a client browser should start up
       And a client should be able to visit /
-      
+
       # Title
       # -----
-      And a client should be able to see an element tagged title
-      
+      And a client should be able to see an element tagged title containing the text value Demos
+
       # Root Elements
       # ---------
-      And a client should be able to see an element tagged h1
-      And a client should be able to see an element tagged a[href="/artprize"]
-      And a client should be able to see an element tagged a[href="/style-guide"]
+      And a client should be able to see an element tagged h1[data-type="demos"] containing the text value Demos
+      And a client should be able to see an element tagged a[href="/artprize"] containing the text value ArtPrize Demo
+      And a client should be able to see an element tagged h1[data-type="style-guide"] containing the text value Style Guide
+      And a client should be able to see an element tagged a[href="/style-guide"] containing the text value Style Guide
 
       And a client browser should shut down
     Then the server should shut down
-      

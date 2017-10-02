40e0f6476806fdbce09a95f81d88952a7d1cdedb
kendell joseph <kendell@jct.io>
2017-10-02 10:01:39 +0000

    Added development gifs

--- demos/artprize/controllers/viewer/viewer.controller.js
+++ demos/artprize/controllers/viewer/viewer.controller.js
@@ -6,7 +6,7 @@ app.controller(`blockforgeViewer`, [`$scope`, `constants`, function($scope){
   $scope.status      = `Waiting for data...`;
   $scope.robotStatus = `Waiting for data...`;
   $scope.viewers     = `Waiting for data...`;
-  $scope.goodbye     = `Thank you for visiting!`;
+  $scope.goodbye     = `The development process`;
 
   // Robots
   // ------
